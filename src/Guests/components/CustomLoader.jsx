07fad11f8a1246f8bc1b483b8011c9fb6ae7aa70
KudakwasheM <masayakudakwashe@gmail.com>
2023-09-19 16:10:27 +0200

    Loader

--- src/Guests/components/CustomLoader.jsx
+++ src/Guests/components/CustomLoader.jsx
@@ -4,8 +4,8 @@ import { InfinitySpin } from "react-loader-spinner";
 const CustomLoader = () => {
   return (
     <div className="flex flex-col justify-center items-center">
-      <InfinitySpin width="200" color="#4fa94d" />
-      <p className="text-[#4fa94d] text-lg">Loading...</p>
+      <InfinitySpin width="200" color="#00df9a" />
+      <p className="text-[#00df9a] text-lg">Loading...</p>
     </div>
   );
 };
