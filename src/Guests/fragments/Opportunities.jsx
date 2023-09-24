bef20cff2ee0c7dd4d91c1defb9b3f37e2f8ca4f
KudakwasheM <masayakudakwashe@gmail.com>
2023-09-24 14:01:48 +0200

    Links to projects

--- src/Guests/fragments/Opportunities.jsx
+++ src/Guests/fragments/Opportunities.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const Opportunities = () => {
   return (
@@ -12,12 +13,42 @@ const Opportunities = () => {
           ensure the relationship is valuable to both parties.
         </p>
         <div className="grid md:grid-cols-2 lg:grid-cols-3 gap-5 mx-5 lg:mx-20 my-10">
-          <div className="opp farm ">Agriculture</div>
-          <div className="opp mine ">Mining</div>
-          <div className="opp soft ">Technology</div>
-          <div className="opp food ">Food</div>
-          <div className="opp edu ">Education</div>
-          <div className="opp med ">Health</div>
+          <Link
+            to={`/proposals?opportunity=agriculture`}
+            className="opp farm hover:shadow-[rgba(50,50,93,0.25)_0px_6px_12px_-2px,_rgba(0,0,0,0.3)_0px_3px_7px_-3px]"
+          >
+            Agriculture
+          </Link>
+          <Link
+            to={`/proposals?opportunity=mining`}
+            className="opp mine hover:shadow-[rgba(50,50,93,0.25)_0px_6px_12px_-2px,_rgba(0,0,0,0.3)_0px_3px_7px_-3px]"
+          >
+            Mining
+          </Link>
+          <Link
+            to={`/proposals?opportunity=technology`}
+            className="opp soft hover:shadow-[rgba(50,50,93,0.25)_0px_6px_12px_-2px,_rgba(0,0,0,0.3)_0px_3px_7px_-3px]"
+          >
+            Technology
+          </Link>
+          <Link
+            to={`/proposals?opportunity=food`}
+            className="opp food hover:shadow-[rgba(50,50,93,0.25)_0px_6px_12px_-2px,_rgba(0,0,0,0.3)_0px_3px_7px_-3px]"
+          >
+            Food
+          </Link>
+          <Link
+            to={`/proposals?opportunity=education`}
+            className="opp edu hover:shadow-[rgba(50,50,93,0.25)_0px_6px_12px_-2px,_rgba(0,0,0,0.3)_0px_3px_7px_-3px]"
+          >
+            Education
+          </Link>
+          <Link
+            to={`/proposals?opportunity=health`}
+            className="opp med hover:shadow-[rgba(50,50,93,0.25)_0px_6px_12px_-2px,_rgba(0,0,0,0.3)_0px_3px_7px_-3px]"
+          >
+            Health
+          </Link>
         </div>
       </div>
     </div>
