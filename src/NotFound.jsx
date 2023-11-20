83120d0146f726833e4c8b87b3b50f8ada9a4eb3
KudakwasheM <masayakudakwashe@gmail.com>
2023-11-20 15:52:15 +0200

    Not found page

--- src/NotFound.jsx
+++ src/NotFound.jsx
@@ -1,7 +1,20 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const NotFound = () => {
-  return <div>NotFound</div>;
+  return (
+    <div className="h-screen w-screen flex flex-col items-center justify-center text-center">
+      <div className="">
+        <p className="text-2xl text-red-600">Error 404</p>
+        <p className="text-3xl">Page not found</p>
+      </div>
+      <div className="mt-2">
+        <Link to={`/`} className="text-xl underline text-sky-500">
+          Go back
+        </Link>
+      </div>
+    </div>
+  );
 };
 
 export default NotFound;
